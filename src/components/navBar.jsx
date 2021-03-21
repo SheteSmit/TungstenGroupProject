f5f089ad88bd7377d0244a5f150fe5c5551b59eb
ChristianRoque <croquevaldes@hotmail.com>
2021-03-20 21:06:39 -0400

    logo

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar(props) {
   return (
     <header>
       <Navbar expand="lg" variant="light" bg="light">
-        <img className="logo" src="https://i.imgur.com/yDxQ7CZ.png" />
+        <img className="logo" src="https://i.imgur.com/rRTK4EH.png" />
         <Container>
           <Navbar.Brand href="#">Token Faucet</Navbar.Brand>
           <NavItem>Account: {props.account}</NavItem>
