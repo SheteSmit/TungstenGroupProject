17c6dbe6631f921bfb2df739fba944879644db64
ChristianRoque <croquevaldes@hotmail.com>
2021-03-26 16:38:44 -0400

    fixed loop

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,24 +1,17 @@
-import React, { createContext, useState, useEffect } from 'react';
-import ContractAPI from './api/contractAPI';
+import React, { createContext, useState, useEffect } from "react";
+import ContractAPI from "./api/contractAPI";
 
-export const GlobalState = createContext()
-
+export const GlobalState = createContext();
 
 export const DataProvider = ({ children }) => {
-    const [useContract, setUseContract] = useState(false)
+  const [useContract, setUseContract] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {}, []);
 
+  const state = {
+    useContract: [useContract, setUseContract],
+    contractAPI: ContractAPI(useContract),
+  };
 
-    }, [])
-
-    const state = {
-        useContract: [useContract, setUseContract],
-        contractAPI: ContractAPI(useContract)
-    }
-    return (
-        <GlobalState.Provider value={state}>
-            {children}
-        </GlobalState.Provider>
-    )
-}
+  return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
+};
