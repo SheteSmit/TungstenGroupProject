c7616c6586ccd25df753db8540a21cc029daa4ac
hoseacodes <dominique11h@yahoo.com>
2021-03-26 15:10:12 -0500

    Test Demo

---
+++ src/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect } from 'react';
+import ContractAPI from './api/contractAPI';
+
+export const GlobalState = createContext()
+
+
+export const DataProvider = ({ children }) => {
+    const [useContract, setUseContract] = useState(false)
+
+    useEffect(() => {
+
+
+    }, [])
+
+    const state = {
+        useContract: [useContract, setUseContract],
+        contractAPI: ContractAPI(useContract)
+    }
+    return (
+        <GlobalState.Provider value={state}>
+            {children}
+        </GlobalState.Provider>
+    )
+}
