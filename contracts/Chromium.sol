11399ff63c9f6d893bbb8526b80c308725a72ac8
Hugo Martinez <dockingtheclouds@gmail.com>
2021-03-31 09:23:45 -0500

    changed exchangeFunction and added to gitignore file

--- contracts/Chromium.sol
+++ contracts/Chromium.sol
@@ -6,6 +6,11 @@ import './SafeMath.sol';
 
 contract Chromium {
 
+    ERC20 sellToken;
+    ERC20 buyToken;
+
+    mapping(address => uint256) tokenBalance;
+
     event TokensPurchased(
         address account,
         address token,
@@ -18,19 +23,31 @@ contract Chromium {
         uint amount
     );
 
-    function buyTokens(address _tokenAddress, uint256 _amountToSell, uint256 _priceOfSellToken, uint256 _priceOfBuyToken) public payable {
+    /**
+    * @dev function that will allow people to exchange tokens
+    * @param _sellTokenAddress the address of the token the user wants to exchange
+    * @param _amountToSell the amount the user wants to exchange
+    * @param _buyTokenAddress the address of the token the user wants to obtain
+    * @param _priceOfSellToken price of the token that is being sold (obtain by api call to etherscan)
+    * @param _priceOfBuyToken price of the token that is being obtained (obtain by api call to etherscan)
+    */
+    function exchangeTokens(address _sellTokenAddress, uint256 _amountToSell, address _buyTokenAddress, uint256 _priceOfSellToken, uint256 _priceOfBuyToken) public payable {
 
-        token = ERC20(address(_tokenAddress));
+        sellToken = ERC20(address(_sellTokenAddress));
+        buyToken = ERC20(address(_buyTokenAddress));
         uint256 _exchangeRate = SafeMath.mul(_amountToSell, SafeMath.div(_priceOfSellToken, _priceOfBuyToken));
 
         // Require that Chromium has enough tokens
-        require(token.balanceOf(address(this)) >= _exchangeRate);
+        require(tokenBalance[_buyTokenAddress] >= _exchangeRate);
+
+        tokenBalance[_buyTokenAddress] = SafeMath.sub(tokenBalance[_buyTokenAddress], _exchangeRate);
+        tokenBalance[_sellTokenAddress] = SafeMath.add(tokenBalance[_sellTokenAddress], _amountToSell);
 
         // Transfer tokens to the user
-        token.transfer(msg.sender, _exchangeRate);
+        buyToken.transfer(msg.sender, _exchangeRate);
 
         // Emit an event
-        emit TokensPurchased(msg.sender, address(token), _exchangeRate);
+        emit TokensPurchased(msg.sender, _buyTokenAddress, _exchangeRate);
     }
 
 }
