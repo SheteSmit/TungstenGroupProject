9da34d6750ef16373b8c334d369a025333a2545f
hoseacodes <dominique11h@yahoo.com>
2021-04-04 17:01:48 -0500

    Adj Nav

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { Navbar, NavItem, Container } from "react-bootstrap";
+import { Navbar, Nav, NavItem, Container } from "react-bootstrap";
 
 export default function NavBar(props) {
   return (
     <header>
       <Navbar expand="lg" variant="light" bg="light">
         <Container fluid>
-          <Navbar.Brand href="">Cobalt Lend Treasury</Navbar.Brand>
-          <NavItem ><button className="buttonMid" style={{
-            border: "1px solid #f7f7f7",
-            borderRadius: '12px',
-            background: "#49bcf8",
-            color: "white",
-            padding: ".3em .7em",
-            fontSize: "inherit",
-            display: "block",
-            cursor: "pointer",
-            margin: "1em auto"
-          }}
-            onClick={props.openTour}>
-            Take A Tour
+          <Navbar.Brand className="ml-5" href="">Cobalt Lend Treasury</Navbar.Brand>
+          <Nav className="justify-content-end align-items-center">
+
+            <NavItem ><button className="buttonMid mr-5" style={{
+              border: "1px solid #f7f7f7",
+              borderRadius: '12px',
+              background: "#49bcf8",
+              color: "white",
+              padding: ".3em .7em",
+              fontSize: "inherit",
+              display: "block",
+              cursor: "pointer",
+              margin: "1em auto"
+            }}
+              onClick={props.openTour}>
+              Take A Tour
       </button></NavItem>
-          <NavItem className="metaacct">Meta Mask Account: {props.account}</NavItem>
+            <NavItem className="metaacct mr-5">Meta Mask Account: {props.account}</NavItem>
+          </Nav>
+
         </Container>
       </Navbar>
     </header>
