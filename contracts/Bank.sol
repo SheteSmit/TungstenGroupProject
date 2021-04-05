e8dfc37f0a70fa116efc824407b0e6a417aa5a3a
Woodrow Jackson <woodrow.jackson@rocketmail.com>
2021-04-05 12:33:17 -0700

    Change Bank

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -5,18 +5,39 @@ import "./Ownable.sol";
 import "./ERC20.sol";
 import "./SafeMath.sol";
 
+
+
+
+
 contract Bank is Ownable {
     uint256 public rate = 10;
 
-    constructor(address[] memory addresses) public {
-        for (uint256 i = 0; i < addresses.length; i++) {
-            tokensAllowed[addresses[i]] = true;
-        }
+    /**
+     * @dev Added the Struct into from loan contract into the bank sol
+     * 
+     */
+
+    struct Rational{
+        uint256 numerator;
+        uint256 denominator;
     }
 
     /**
-     * @dev Events emitted
+     * @dev  Using variables needed to use theloan function properly
      */
+    address lender;
+    address borrower;
+    Rational public interestRate;
+    uint256 public dueDate;
+    uint256 paymentPeriod;
+
+    uint256 public remainingBalance;
+    uint256 minimumPayment;
+
+    ERC20 token;
+    uint256 collateralPerPayment;
+
+   
 
     event onReceived(address indexed _from, uint256 _amount);
     event onTransfer(
@@ -45,11 +66,43 @@ contract Bank is Ownable {
      */
     mapping(address => uint256) public etherBalance;
 
+    
+
+    
+
+    constructor(address[] _arrayAddress, ERC20 _token,
+
+    address _lender,
+    address _borrower,
+    uint256 interestRateNumerator,
+    uint256 interestRateDenominator,
+    uint256 _paymentPeriod,
+    uint256 _minimumPayment,
+    uint256 principal,
+    uint256 units) 
+    public {  
+    lender = _lender;
+    borrower = _borrower;
+    interestRate = Rational(interestRateNumerator, interestRateDenominator);
+    paymentPeriod = _paymentPeriod;
+    minimumPayment = _minimumPayment;
+    remainingBalance = principal;
+    token = _token;
+    collateralPerPayment = units;
+    
+    uint256 x = minimumPayment * collateralPerPayment;
+        require(x / collateralPerPayment == minimumPayment,
+            "minimumPayment * collateralPerPayment overflows");
+
+        dueDate = now + paymentPeriod;
+
+    
+    }
+
     /**
      * @dev method that will withdraw tokens from the bank if the caller
      * has tokens in the bank
      */
-
     function withdrawTokens(address _tokenAddress, uint256 _amount) external {
         //Check if token is not supported by bank
         require(tokensAllowed[_tokenAddress] == true, "Token is not supported");
@@ -169,16 +222,12 @@ contract Bank is Ownable {
      * @dev method that will show the balance that the caller has
      * for a certain token
      */
-    function balanceOf() public view returns (uint256) {
-        return etherBalance[msg.sender];
-    }
-
-    function balanceOfToken(address _tokenAddress)
-        public
-        view
-        returns (uint256)
-    {
-        return tokenOwnerBalance[_tokenAddress][msg.sender];
+    function balanceOf(address _tokenAddress) public view returns (uint256) {
+        if (_tokenAddress == 0x0) {
+            return etherBalance[msg.sender];
+        } else {
+            return tokenOwnerBalance[_tokenAddress][msg.sender];
+        }
     }
 
     /**
@@ -188,4 +237,143 @@ contract Bank is Ownable {
     receive() external payable {
         emit onReceived(msg.sender, msg.value);
     }
+
+
+
+    // All loan contract functions Here
+
+    /**
+     * @dev 
+     * 
+     */
+    function multiply(uint256 x, Rational r) internal pure returns (uint256) {
+        return x * r.numerator / r.denominator;
+    }
+
+    /**
+     * @dev 
+     * 
+     */
+
+      function calculateComponents(uint256 amount)
+        internal
+        view
+        returns (uint256 interest, uint256 principal)
+    {
+        interest = multiply(remainingBalance, interestRate);
+        require(amount >= interest);
+        principal = amount - interest;
+        return (interest, principal);
+    }
+
+
+     /**
+     * @dev he loan is parameterized with collateralPerPayment, 
+     *which represents the amount of collateral that will be returned
+     * or forfeited based on a minimumPayment. If the borrower pays an 
+     *amount different than the minimum, the amount of collateral returned 
+     * is adjusted proportionally.
+     * 
+     */
+
+     function calculateCollateral(uint256 payment)
+        internal
+        view
+        returns (uint256 units)
+    {
+        uint256 product = collateralPerPayment * payment;
+        require(product / collateralPerPayment == payment, "payment causes overflow");
+        units = product / minimumPayment;
+        return units;
+    }
+
+
+     /**
+     * @dev a symmetry between accepting loan payments and handling missed payments. 
+     * In both cases, there is an adjustment to the remaining principal balance and a 
+     * corresponding transfer of tokens. The only difference is that the tokens are 
+     * returned to the borrower after a payment, but they are forfeited to the lender 
+     * after a missed payment
+     * 
+     *Additional Note: the code above does the token transfer last, which follows the 
+     * Checks-Effects-Interactions pattern to avoid potential reentrancy vulnerabilities
+     */
+     function processPeriod(uint256 interest, uint256 principal, address recipient) internal {
+        uint256 units = calculateCollateral(interest+principal);
+
+        remainingBalance -= principal;
+
+        dueDate += paymentPeriod;
+
+        require(token.transfer(recipient, units));
+    }
+
+      /**
+     * @dev 
+     * 
+     */
+     function makePayment() public payable {
+        require(now <= dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(msg.value);
+
+        require(principal <= remainingBalance);
+        require(msg.value >= minimumPayment || principal == remainingBalance);
+
+        processPeriod(interest, principal, borrower);
+    }
+
+
+
+    
+     
+
+      /**
+     * @dev  computes the principal component of the missed payment. 
+     * This assumes the payment was the minimum amount, which is true
+     * for all but, possibly, the last payment. The conditional handles 
+     * the boundary condition when the principal remaining is less than 
+     * the principal component of a minimum payment.
+     * 
+     */
+    function missedPayment() public {
+        require(now > dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(minimumPayment);
+
+        if (principal > remainingBalance) {
+            principal = remainingBalance;
+        }
+
+
+
+    
+      /**
+     * @dev  smart contract allows borrowers to pay more than the minimum, 
+     * which will ultimately lead to less total paid because of avoided interest.
+     * If used, this feature will lead to excess collateral owned by the loan contract
+     * after it’s been fully paid off. This collateral belongs to the borrower. 
+     * The simplest way to handle that is to allow excess tokens to be claimed when the remainingBalance is zero:
+     * 
+     */
+     function returnCollateral() public {
+        require(remainingBalance == 0);
+
+        uint256 amount = token.balanceOf(this);
+        require(token.transfer(borrower, amount));
+    }
+
+
+
+// Big problem Bank allready has an withdraw but this loan contract has a withdrawal as well.
+    function withdraw() public {
+    lender.transfer(address(this).balance);
+    }
+        processPeriod(interest, principal, lender);
+    }
+
 }
