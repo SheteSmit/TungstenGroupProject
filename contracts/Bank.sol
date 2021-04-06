8df860a1e1cbf84c7cc4b1b3668cddf0cea366bf
ChristianRoque <croquevaldes@hotmail.com>
2021-04-05 23:53:22 -0400

    merged the lending protocol with bank

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -9,10 +9,40 @@ contract Bank is Ownable {
     uint256 public rate = 10;
     ERC20 token;
 
-    constructor(address[] memory addresses) public {
+    /**
+     * @dev mapping used to store all loan information with the key of borrower address
+     *  and value of Loan struct with all loan information
+     */
+    mapping(address => Loan) loanBook;
+
+    constructor(address[] memory addresses, address _CBLT) public {
         for (uint256 i = 0; i < addresses.length; i++) {
             tokensAllowed[addresses[i]] = true;
         }
+        token = ERC20(_CBLT);
+    }
+
+    /**
+     * @dev Struct created so save all loan information
+     */
+    struct Loan {
+        address borrower;
+        uint256 dueDate;
+        Rational interestRate;
+        uint256 paymentPeriod;
+        uint256 remainingBalance;
+        uint256 minimumPayment;
+        uint256 collateralPerPayment;
+        bool active;
+        bool initialized;
+    }
+
+    /**
+     * @dev Struct used to calculate interest rate
+     */
+    struct Rational {
+        uint256 numerator;
+        uint256 denominator;
     }
 
     /**
@@ -187,5 +217,200 @@ contract Bank is Ownable {
 
     receive() external payable {
         emit onReceived(msg.sender, msg.value);
+    }
+
+    // ****************************** Lending **********************************
+
+    /**
+     * @dev Recalculates interest and also conducts check and balances
+     */
+    function newLoan(
+        uint256 interestRateNumerator,
+        uint256 interestRateDenominator,
+        uint256 _paymentPeriod,
+        uint256 _minimumPayment,
+        uint256 principal,
+        uint256 units
+    ) public {
+        // Needs user approve on transfer funds amount
+        // token.approve(address(this), _minimumPayment);
+
+        require(loanBook[msg.sender].initialized == false);
+        require(
+            token.transferFrom(msg.sender, address(this), units * 12) == true,
+            "Payment was not approved."
+        );
+        loanBook[msg.sender] = Loan(
+            msg.sender,
+            (block.timestamp + _paymentPeriod),
+            Rational(interestRateNumerator, interestRateDenominator),
+            _paymentPeriod,
+            principal,
+            _minimumPayment,
+            units,
+            false,
+            true
+        );
+
+        uint256 x = _minimumPayment * units;
+        require(
+            x / units == _minimumPayment,
+            "minimumPayment * collateralPerPayment overflows"
+        );
+    }
+
+    /**
+     * @dev Prevents overflows
+     *
+     */
+    function multiply(uint256 x, Rational memory r)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (x * r.numerator) / r.denominator;
+    }
+
+    /**
+     * @dev Recalculates interest and also conducts check and balances
+     *
+     */
+    function calculateComponents(uint256 amount)
+        internal
+        view
+        returns (uint256 interest, uint256 principal)
+    {
+        interest = multiply(
+            loanBook[msg.sender].remainingBalance,
+            loanBook[msg.sender].interestRate
+        );
+        require(amount >= interest);
+        principal = amount - interest;
+        return (interest, principal);
+    }
+
+    /**
+     * @dev he loan is parameterized with collateralPerPayment,
+     *which represents the amount of collateral that will be returned
+     * or forfeited based on a minimumPayment. If the borrower pays an
+     *amount different than the minimum, the amount of collateral returned
+     * is adjusted proportionally.
+     *
+     */
+    function calculateCollateral(uint256 payment)
+        internal
+        view
+        returns (uint256 units)
+    {
+        uint256 product = loanBook[msg.sender].collateralPerPayment * payment;
+        require(
+            product / loanBook[msg.sender].collateralPerPayment == payment,
+            "payment causes overflow"
+        );
+        units = product / loanBook[msg.sender].minimumPayment;
+        return units;
+    }
+
+    /**
+     * @dev a symmetry between accepting loan payments and handling missed payments.
+     * In both cases, there is an adjustment to the remaining principal balance and a
+     * corresponding transfer of tokens. The only difference is that the tokens are
+     * returned to the borrower after a payment, but they are forfeited to the lender
+     * after a missed payment
+     *
+     *Additional Note: the code above does the token transfer last, which follows the
+     * Checks-Effects-Interactions pattern to avoid potential reentrancy vulnerabilities
+     */
+    function processPeriod(
+        uint256 interest,
+        uint256 principal,
+        address recipient
+    ) internal {
+        if (recipient == 0x0000000000000000000000000000000000000000) {
+            uint256 units = calculateCollateral(interest + principal);
+
+            loanBook[msg.sender].remainingBalance -= principal;
+
+            loanBook[msg.sender].dueDate += loanBook[msg.sender].paymentPeriod;
+        } else {
+            uint256 units = calculateCollateral(interest + principal);
+
+            loanBook[msg.sender].remainingBalance -= principal;
+
+            loanBook[msg.sender].dueDate += loanBook[msg.sender].paymentPeriod;
+
+            require(token.transfer(recipient, units));
+        }
+    }
+
+    /**
+     * @dev Function for the user to make a payment with ETH
+     *
+     */
+    function makePayment() public payable {
+        require(block.timestamp <= loanBook[msg.sender].dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(msg.value);
+
+        require(principal <= loanBook[msg.sender].remainingBalance);
+        require(
+            msg.value >= loanBook[msg.sender].minimumPayment ||
+                principal == loanBook[msg.sender].remainingBalance
+        );
+
+        processPeriod(interest, principal, msg.sender);
+    }
+
+    /**
+     * @dev  computes the principal component of the missed payment.
+     * This assumes the payment was the minimum amount, which is true
+     * for all but, possibly, the last payment. The conditional handles
+     * the boundary condition when the principal remaining is less than
+     * the principal component of a minimum payment.
+     *
+     */
+    function missedPayment() public {
+        require(block.timestamp > loanBook[msg.sender].dueDate);
+
+        uint256 interest;
+        uint256 principal;
+        (interest, principal) = calculateComponents(
+            loanBook[msg.sender].minimumPayment
+        );
+
+        if (principal > loanBook[msg.sender].remainingBalance) {
+            principal = loanBook[msg.sender].remainingBalance;
+        }
+
+        processPeriod(
+            interest,
+            principal,
+            0x0000000000000000000000000000000000000000
+        );
+    }
+
+    /**
+     * @dev  smart contract allows borrowers to pay more than the minimum,
+     * which will ultimately lead to less total paid because of avoided interest.
+     * If used, this feature will lead to excess collateral owned by the loan contract
+     * after it’s been fully paid off. This collateral belongs to the borrower.
+     * The simplest way to handle that is to allow excess tokens to be claimed when the remainingBalance is zero:
+     *
+     */
+    function returnCollateral() public {
+        require(loanBook[msg.sender].remainingBalance == 0);
+
+        uint256 amount = token.balanceOf(address(this));
+        require(token.transfer(msg.sender, amount));
+    }
+
+    /**
+     * @dev Deletes loan instance once the user has paid his first loan in full
+     */
+    function cleanSlate() public {
+        require(loanBook[msg.sender].remainingBalance == 0);
+        delete loanBook[msg.sender];
     }
 }
--- contracts/Chromium.sol
+++ contracts/Chromium.sol
@@ -53,23 +53,24 @@ contract Chromium {
     ) external payable {
         // pulls the price of both tokens from an oracle using call without abi
         (bool result, bytes memory data) =
-        oracleAddress.call(
-            abi.encodeWithSignature(
-                "priceOfPair(address,address)",
-                _sellToken,
-                _buyToken
-            )
-        );
+            oracleAddress.call(
+                abi.encodeWithSignature(
+                    "priceOfPair(address,address)",
+                    _sellToken,
+                    _buyToken
+                )
+            );
+
         // Decode bytes data
         (uint256 sellTokenValue, uint256 buyTokenValue) =
-        abi.decode(data, (uint256, uint256));
+            abi.decode(data, (uint256, uint256));
 
         //Calculate tokens bought
         uint256 buyingAmount =
-        SafeMath.mul(
-            _sellAmount,
-            SafeMath.div(sellTokenValue, buyTokenValue)
-        );
+            SafeMath.mul(
+                _sellAmount,
+                SafeMath.div(sellTokenValue, buyTokenValue)
+            );
 
         // checks to see if there are enough tokens in the contract to make the exchange
         require(buyingAmount <= treasury.totalTokenSupply(_buyToken));
@@ -89,32 +90,29 @@ contract Chromium {
      * @param _sellAmount amount of tokens that will be sold
      */
     function exchangeTokenForEth(address _sellToken, uint256 _sellAmount)
-    external
-    payable
+        external
+        payable
     {
         address ethAddress = 0x0000000000000000000000000000000000000000;
 
         // pulls the price of both tokens from an oracle using call without abi
         (bool result, bytes memory data) =
-        oracleAddress.call(
-            abi.encodeWithSignature(
-                "priceOfPair(address,address)",
-                _sellToken,
-                ethAddress
-            )
-        );
+            oracleAddress.call(
+                abi.encodeWithSignature(
+                    "priceOfPair(address,address)",
+                    _sellToken,
+                    ethAddress
+                )
+            );
         // Decode bytes data
         (uint256 sellTokenValue, uint256 ethTokenValue) =
-        abi.decode(data, (uint256, uint256));
+            abi.decode(data, (uint256, uint256));
 
         uint256 ethAmount =
-        SafeMath.mul(
-            _sellAmount,
-            SafeMath.div(
-                sellTokenValue,
-                ethTokenValue
-            )
-        );
+            SafeMath.mul(
+                _sellAmount,
+                SafeMath.div(sellTokenValue, ethTokenValue)
+            );
 
         // checks to see if the contract has enough eth
         require(ethAmount <= ethSupply, "You don't have enough tokens");
@@ -136,28 +134,24 @@ contract Chromium {
      * @param _buyToken the token that you want to buy
      */
     function exchangeEthForToken(address _buyToken) external payable {
-
         address ethAddress = 0x0000000000000000000000000000000000000000;
 
         // pulls the price of both tokens from an oracle using call without abi
         (bool result, bytes memory data) =
-        oracleAddress.call(
-            abi.encodeWithSignature(
-                "priceOfPair(address,address)",
-                ethAddress,
-                _buyToken
-            )
-        );
+            oracleAddress.call(
+                abi.encodeWithSignature(
+                    "priceOfPair(address,address)",
+                    ethAddress,
+                    _buyToken
+                )
+            );
 
         // Decode bytes data
         (uint256 ethValue, uint256 buyTokenValue) =
-        abi.decode(data, (uint256, uint256));
+            abi.decode(data, (uint256, uint256));
 
         uint256 tokenAmount =
-        SafeMath.mul(
-            msg.value,
-            SafeMath.div(ethValue, buyTokenValue)
-        );
+            SafeMath.mul(msg.value, SafeMath.div(ethValue, buyTokenValue));
 
         // checks to see if there are enough tokens in contract to make exchange
         require(tokenAmount <= balancePerToken[_buyToken]);
@@ -175,8 +169,8 @@ contract Chromium {
      * @dev this will accept erc20 tokens to be added to the contracts liquidity pool
      */
     function addLiquidity(address _tokenAddress, uint256 _tokenAmount)
-    external
-    payable
+        external
+        payable
     {
         //Check if token is not supported by bank
         require(allowedTokens[_tokenAddress] == true, "Token is not supported");
@@ -190,7 +184,7 @@ contract Chromium {
 
         require(
             sellToken.transferFrom(msg.sender, address(this), _tokenAmount) ==
-            true,
+                true,
             "Transfer not complete"
         );
 
@@ -216,10 +210,10 @@ contract Chromium {
 
     function testCall() public payable returns (uint256 value) {
         (bool result, bytes memory data) =
-        oracleAddress.call(abi.encodeWithSignature("testConnection()"));
+            oracleAddress.call(abi.encodeWithSignature("testConnection()"));
 
         (uint256 sellTokenValue, uint256 buyTokenValue) =
-        abi.decode(data, (uint256, uint256));
+            abi.decode(data, (uint256, uint256));
         return sellTokenValue + buyTokenValue;
     }
 
