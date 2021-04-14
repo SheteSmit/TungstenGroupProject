b8e0454971f1e9ea4fce351824117324240fbd40
Hugo Martinez <dockingtheclouds@gmail.com>
2021-04-14 12:06:26 -0500

    updated the getExpectedReturn and exchangetoken functions

--- contracts/Chromium.sol
+++ contracts/Chromium.sol
@@ -16,23 +16,23 @@ pragma solidity >=0.4.22 <0.9.0;
 */
 
 import "./interfaces/UniversalERC20.sol";
+import './interfaces/Ownable.sol';
 import "./interfaces/IOneSplit.sol";
 import './ExchangeOracle.sol';
 import "./Bank.sol";
 
-contract Chromium {
+contract Chromium is Ownable{
     using UniversalERC20 for IERC20;
 
-    mapping(IERC20 => uint256) public balancePerToken;
     mapping(IERC20 => bool) public allowedTokens; // tokens that are allowed to be exchanged
     address oracleAddress;
 
     Bank treasury;
     ExchangeOracle oracle;
-    IOneSplit public oneSplitImpl; // pass in 1inch protocol contract address
 
     IERC20 private constant ZERO_ADDRESS = IERC20(0x0000000000000000000000000000000000000000); // eth address substitute
     IERC20 private constant ETH_ADDRESS = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE); // eth address substitute
+    IOneSplit private constant oneSplitImpl = IOneSplit(0xc3037b2A1a9E9268025FF6d45Fe7095436446D52); // sets 1inch protocol
 
     event depositToken(address indexed _from, uint256 _amount);
     event onTransfer(
@@ -57,11 +57,16 @@ contract Chromium {
     }
 
     /**
-    * this sets the 1inch protocol
-    * @param _oneAuditImpl the 1inch protol address (0xC586BeF4a0992C495Cf22e1aeEE4E446CECDee0E)
+    * this sets the treasury, and oracle
     */
-    function setOneProtocol(IOneSplit _oneAuditImpl) public {
-        oneSplitImpl = _oneAuditImpl;
+
+    function setTreasury(address payable _treasury) public onlyOwner {
+        treasury = Bank(_treasury);
+    }
+
+    function setOracle(address _oracle) public onlyOwner {
+        oracle = ExchangeOracle(_oracle);
+        oracleAddress = _oracle;
     }
 
     /**
@@ -73,36 +78,32 @@ contract Chromium {
      * works like granularity, higly affects gas usage. Should be called offchain,
      * but could be called onchain if user swaps not his own funds, but this is still considered as not safe.
      * @param flags (uint256) Flags for enabling and disabling some features, default 0
-     * @param destTokenEthPriceTimesGasPrice (uint256) destToken price to ETH multiplied by gas price
     */
-    function getExpectedReturnWithGas(
+    function getExpectedReturn(
         IERC20 fromToken,
         IERC20 destToken,
         uint256 amount,
         uint256 parts,
-        uint256 flags, // See constants in IOneSplit.sol
-        uint256 destTokenEthPriceTimesGasPrice
+        uint256 flags // See constants in IOneSplit.sol
     )
     public
     view
     returns(
         uint256 returnAmount,
-        uint256 estimateGasAmount,
         uint256[] memory distribution
     )
     {
-        return oneSplitImpl.getExpectedReturnWithGas(
+        return oneSplitImpl.getExpectedReturn(
             fromToken,
             destToken,
             amount,
             parts,
-            flags,
-            destTokenEthPriceTimesGasPrice
+            flags
         );
     }
 
     /**
-     * @dev Swap `_sellAmount` of `_sellToken` to `_buyToken`
+     * @dev Swap `amount` of `fromToken` to 'destToken`
      * @param fromToken (IERC20) Address of token or `address(0)` for Ether
      * @param destToken (IERC20) Address of token or `address(0)` for Ether
      * @param amount (uint256) Amount for `fromToken`
@@ -117,7 +118,98 @@ contract Chromium {
         uint256 minReturn,
         uint256[] memory distribution,
         uint256 flags
-    ) external payable returns (uint256 _amountReturned) {
+    ) external payable {
+        // makes sure tokens aren't the same and amount is greater than 0
+        require(fromToken != destToken && amount > 0, "Unable to swap");
+        // makes sure msg.value is only being used for eth
+        require((msg.value != 0) == fromToken.isETH(), "msg.value can only be used for eth");
+
+        if (_exchangeWithChromium(fromToken, destToken, minReturn)) {
+            fromToken.universalApprove(address(this), amount);
+
+            fromToken.universalTransferFrom(msg.sender, address(treasury), amount);
+            destToken.universalTransferFrom(address(treasury), msg.sender, minReturn);
+
+            emit tokensExchanged(address(fromToken), amount, address(destToken), minReturn);
+        } else {
+            uint fromTokenBalanceBefore = SafeMath.sub(fromToken.universalBalanceOf(address(this)), msg.value);
+            uint destTokenBalanceBefore = destToken.universalBalanceOf(address(this));
+
+            fromToken.universalTransferFromSenderToThis(amount);
+            fromToken.universalApprove(address(oneSplitImpl), amount);
+
+            oneSplitImpl.swap{value:msg.value}(
+                fromToken,
+                destToken,
+                amount,
+                minReturn,
+                distribution,
+                flags
+            );
+
+            uint fromTokenBalanceAfter = fromToken.universalBalanceOf(address(this));
+            uint destTokenBalanceAfter = destToken.universalBalanceOf(address(this));
+            uint returnAmount = SafeMath.sub(destTokenBalanceAfter, destTokenBalanceBefore);
+
+            require(returnAmount >= minReturn, "actual return amount is less than min return amount");
+            destToken.universalTransfer(msg.sender, returnAmount);
+
+            if (fromTokenBalanceAfter > fromTokenBalanceBefore) {
+                fromToken.universalTransfer(msg.sender, SafeMath.sub(fromTokenBalanceAfter, fromTokenBalanceBefore));
+            }
+            emit tokensExchanged(address(fromToken), amount, address(destToken), minReturn);
+        }
+    }
+
+    /**
+     * @dev this will accept erc20 tokens to be added to the contracts liquidity pool
+     */
+    function addLiquidity(IERC20 _token, uint256 _tokenAmount)
+    external
+    payable
+    {
+        //Check if token is not supported by bank
+        require(allowedTokens[_token] == true, "Token is not supported");
+
+        _token.approve(address(this), _tokenAmount);
+        _token.universalTransferFrom(msg.sender, address(treasury), _tokenAmount);
+        emit depositToken(msg.sender, _tokenAmount);
+    }
+
+    /**
+     * @dev allows for tokens to be added to the exchange
+     * this needs to be done before adding any liquidity for the token
+     */
+    function _allowToken(IERC20 _token) public onlyOwner {
+        allowedTokens[_token] = true;
+    }
+
+    /**
+     * @dev method determines if chromium is able to make the exchange, if chromium cant make the exchange
+     * then it will return false and the 1inch protocol will complete the exchange
+     * @param _minReturn is the _minReturn that is returned from "getExpectedReturnWithGas"
+    */
+    function _exchangeWithChromium(IERC20 _fromToken, IERC20 _destToken, uint _minReturn) internal view returns (bool) {
+        if (allowedTokens[_fromToken] == true && allowedTokens[_destToken] == true) {
+            if (_minReturn <= treasury.totalTokenSupply(address(_destToken))) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    function testCall() public payable returns (uint256 value) {
+        (bool result, bytes memory data) =
+        oracleAddress.call(abi.encodeWithSignature("testConnection()"));
+
+        if(result) {
+            (uint256 sellTokenValue, uint256 buyTokenValue) =
+            abi.decode(data, (uint256, uint256));
+            value = sellTokenValue + buyTokenValue;
+        }
 
         // pulls the price of both tokens from an oracle using call without abi
         // (bool result, bytes memory data) =
@@ -139,78 +231,6 @@ contract Chromium {
         //         _sellAmount,
         //         SafeMath.div(sellTokenValue, buyTokenValue)
         //     );
-
-        if (_exchangeWithChromium(fromToken, destToken, amount)) {
-            // checks to see if there are enough tokens in the contract to make the exchange
-            require(minReturn <= treasury.totalTokenSupply(address(destToken)));
-
-            fromToken.universalTransferFrom(msg.sender, address(treasury), amount);
-            balancePerToken[fromToken] = SafeMath.add(balancePerToken[fromToken], amount);
-
-            destToken.universalTransfer(msg.sender, minReturn);
-            balancePerToken[destToken] = SafeMath.sub(balancePerToken[destToken], minReturn);
-
-            emit tokensExchanged(address(fromToken), amount, address(destToken), minReturn);
-            _amountReturned = minReturn;
-        } else {
-            _amountReturned = oneSplitImpl.swap(fromToken, destToken, amount, minReturn, distribution, flags);
-            emit tokensExchanged(address(fromToken), amount, address(destToken), minReturn);
-        }
-    }
-
-
-    /**
-     * @dev this will accept erc20 tokens to be added to the contracts liquidity pool
-     */
-    function addLiquidity(IERC20 _token, uint256 _tokenAmount)
-    external
-    payable
-    {
-        //Check if token is not supported by bank
-        require(allowedTokens[_token] == true, "Token is not supported");
-        _token.approve(address(this), _tokenAmount);
-
-        balancePerToken[_token] = SafeMath.add(
-            balancePerToken[_token],
-            _tokenAmount
-        );
-
-        _token.universalTransferFrom(msg.sender, address(this), _tokenAmount);
-        emit depositToken(msg.sender, _tokenAmount);
-    }
-
-    /**
-     * @dev allows for tokens to be added to the exchange
-     * this needs to be done before adding any liquidity for the token
-     */
-    function _allowToken(IERC20 _token) public {
-        allowedTokens[_token] = true;
-    }
-
-    /**
-     * @dev method determines if chromium is able to make the exchange, if chromium cant make the exchange
-     * then it will return false and the 1inch protocol will complete the exchange
-     * @param _minReturn is the _minReturn that is returned from "getExpectedReturnWithGas"
-    */
-    function _exchangeWithChromium(IERC20 _sellToken, IERC20 _buyToken, uint _minReturn) internal view returns (bool) {
-        if (allowedTokens[_sellToken] == true && allowedTokens[_buyToken] == true) {
-            if (balancePerToken[_buyToken] >= _minReturn) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    function testCall() public payable returns (uint256 value) {
-        (bool result, bytes memory data) =
-        oracleAddress.call(abi.encodeWithSignature("testConnection()"));
-
-        (uint256 sellTokenValue, uint256 buyTokenValue) =
-        abi.decode(data, (uint256, uint256));
-        return sellTokenValue + buyTokenValue;
     }
 
     // fallback function
