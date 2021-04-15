f6904939de50ca9f8df4f2a5ee523b836c8b8a7e
hoseacodes <dominique11h@yahoo.com>
2021-04-15 17:15:25 -0500

    requirements

--- contracts/NFTScore.sol
+++ contracts/NFTScore.sol
@@ -1,3 +1,23 @@
+/*
+name
+symbol
+Risk Factor
+# of Collateral
+Past & Current Loans:
+Loan Amount
+Amount Remaining
+Interest Rate
+Payment Details
+Time Period on Payments
+Type of currency
+USD Value
+Total number of payments/loans
+Total Interest
+# of Voters - Payout Amount
+Any failed loans
+
+Ask: Do we need to make info public when loan is missed? Isnt it better to just show the voters all the info upfront? What kind of info should we show at the start vs make public?
+*/
 pragma solidity ^0.8.0;
 
 contract ERC721 {
