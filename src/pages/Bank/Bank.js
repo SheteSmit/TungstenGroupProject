3dc7c930a5d66b61d658f61f3b7733e7093879a8
varunsub <varun.subramanyan@gmail.com>
2021-04-20 10:15:19 -0700

    Updated the bank file

--- src/pages/Bank/Bank.js
+++ src/pages/Bank/Bank.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 import Container from '../../components/Container';
 import { useForm } from 'react-hook-form';
-
+import { Button, TextField } from '@material-ui/core';
 export default function Bank() {
   const { register, handleSubmit } = useForm();
   async function submit(data) {
     switch (data.action) {
       case 'deposit': {
+        break;
       }
       case 'withdraw': {
+        break;
       }
       default:
         return;
@@ -17,6 +19,63 @@ export default function Bank() {
   return (
     <Container>
       <form onSubmit={handleSubmit(submit)}>
+        <SpaceAroundRow>
+          <Button variant="outlined">Scoring Token</Button>
+          <Spacer />
+          Cobalt Lend
+          <Spacer />
+          <Button variant="outlined">Connect Wallet</Button>
+        </SpaceAroundRow>
+        <SpaceAroundRow>
+          <Col>
+            <TextField
+              variant="outlined"
+              disabled
+              label="Balance"
+              defaultValue="10"
+              size="small"
+            />
+            <TextField
+              variant="outlined"
+              disabled
+              label="Staking Rewards"
+              defaultValue="10"
+              size="small"
+            />
+            <TextField
+              variant="outlined"
+              disabled
+              label="Outstand Loans"
+              defaultValue="10"
+              size="small"
+            />
+            <TextField
+              variant="outlined"
+              disabled
+              label="Loan Proposals Pending"
+              defaultValue="10"
+              size="small"
+            />
+          </Col>
+          <img src="CobaltLogo.jpg" style={{ width: '30%' }} alt="logo" />
+          <Col>
+            <TextField
+              variant="outlined"
+              disabled
+              label="Loan Status"
+              defaultValue="10"
+              size="small"
+            />
+            <TextField
+              variant="outlined"
+              disabled
+              label="Outstanding Loan Status"
+              defaultValue="10"
+              size="small"
+            />
+          </Col>
+        </SpaceAroundRow>
+        <SpaceAroundRow></SpaceAroundRow>
         <input
           className="form-input"
           type="number"
@@ -31,3 +90,26 @@ export default function Bank() {
     </Container>
   );
 }
+
+const SpaceAroundRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 2% 0 2% 0;
+`;
+const CenterRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Spacer = styled.div`
+  display: inline-block;
+  width: 1px;
+`;
+
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
