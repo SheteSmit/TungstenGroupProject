fbb43ef7ecdda44170300f72fc55ec588361b913
ChristianRoque <croquevaldes@hotmail.com>
2021-06-24 16:16:58 -0400

    added modifier to control valid entries, added struct to keep track on ticket date creation, termination status, winners and participants, created a function for devs to create a ticket with a set amount of winners and participants

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 import "./interfaces/Ownable.sol";
@@ -12,6 +13,8 @@ contract Lottery {
 
     bool public lotteryStatus;
 
+    uint256 flatTicketPrice;
+
     function setAPI(address _newAPI) public {
         API = _newAPI;
     } // Only dev contract // Deployed on Binance
@@ -20,10 +23,72 @@ contract Lottery {
         lotteryStatus = _status;
     } // Only dev contract // Deployed on Binance
 
-    // ****************************** Lottery ******************************
+    function setTicketPrice(uint256 _amount) public {
+        flatTicketPrice = _amount;
+    } // Only dev contract // Deployed on Binance
 
-    modifier validEntry() {
+    // ****************************** Lottery - Staking ******************************
+
+    uint256 lotteryTicketCap;
+
+    struct Lotteryticket {
+        address[] participants;
+        address[] winners;
+        uint256 dateCreated;
+        bool ended;
+    }
+
+    mapping(uint256 => Lotteryticket) lotteryBook;
+
+    function getLotteryParticipants(uint256 _lotteryTicket)
+        public
+        view
+        returns (address[] memory)
+    {
+        return lotteryBook[_lotteryTicket].participants;
+    }
+
+    function getLotteryWinners(uint256 _lotteryTicket)
+        public
+        view
+        returns (address[] memory)
+    {
+        return lotteryBook[_lotteryTicket].winners;
+    }
+
+    modifier validEntry(uint256 _lotteryTicket) {
         require(lotteryStatus == true, "Lottery is currently offline");
+        require(
+            lotteryBook[_lotteryTicket].dateCreated > 0,
+            "Ticket does not exist"
+        );
+        require(
+            lotteryBook[_lotteryTicket].ended == false,
+            "Lottery ticket instance ended"
+        );
+        require(
+            msg.value >= flatTicketPrice,
+            "Value sent is less than ticket price"
+        );
         _;
+    }
+
+    function createTicket(uint256 _numParticipants, uint256 _numWinners)
+        public
+    {
+        lotteryBook[lotteryTicketCap] = Lotteryticket(
+            new address[](_numParticipants),
+            new address[](_numWinners),
+            block.timestamp,
+            false
+        );
+    } // Only dev contract // Deployed on Binance
+
+    function buyLotteryTicket(uint256 _lotteryTicket)
+        public
+        payable
+        validEntry(_lotteryTicket)
+    {
+        lotteryBook[_lotteryTicket].participants.push(msg.sender);
     }
 }
