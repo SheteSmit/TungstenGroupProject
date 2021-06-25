14b81735a95a97cb846ded36d91ab8bc188a2db9
ChristianRoque <croquevaldes@hotmail.com>
2021-06-25 00:13:49 -0400

    finished lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -13,7 +13,7 @@ contract Lottery {
 
     bool public lotteryStatus;
 
-    uint256 flatTicketPrice;
+    uint256 totalFeeBalance;
 
     function setAPI(address _newAPI) public {
         API = _newAPI;
@@ -23,10 +23,6 @@ contract Lottery {
         lotteryStatus = _status;
     } // Only dev contract // Deployed on Binance
 
-    function setTicketPrice(uint256 _amount) public {
-        flatTicketPrice = _amount;
-    } // Only dev contract // Deployed on Binance
-
     // ****************************** Lottery - Staking ******************************
 
     uint256 lotteryTicketCap;
@@ -34,8 +30,10 @@ contract Lottery {
     struct Lotteryticket {
         address[] participants;
         address[] winners;
-        uint256 dateCreated;
-        bool ended;
+        uint256 lotteryType;
+        uint256 dateLimit;
+        uint256 fee;
+        bool pickedWinners;
     }
 
     mapping(uint256 => Lotteryticket) lotteryBook;
@@ -59,36 +57,79 @@ contract Lottery {
     modifier validEntry(uint256 _lotteryTicket) {
         require(lotteryStatus == true, "Lottery is currently offline");
         require(
-            lotteryBook[_lotteryTicket].dateCreated > 0,
+            lotteryBook[_lotteryTicket].dateLimit < block.timestamp,
             "Ticket does not exist"
         );
+
         require(
-            lotteryBook[_lotteryTicket].ended == false,
-            "Lottery ticket instance ended"
-        );
-        require(
-            msg.value >= flatTicketPrice,
+            msg.value >= lotteryBook[_lotteryTicket].fee,
             "Value sent is less than ticket price"
         );
         _;
     }
 
-    function createTicket(uint256 _numParticipants, uint256 _numWinners)
-        public
-    {
+    function createTicket(
+        uint256 _numParticipants,
+        uint256 _numWinners,
+        uint256 _feePrice,
+        uint256 _dateLimit,
+        uint256 _lotteryType
+    ) public {
+        // Staking _numParticipants must be 0
         lotteryBook[lotteryTicketCap] = Lotteryticket(
             new address[](_numParticipants),
             new address[](_numWinners),
-            block.timestamp,
+            _lotteryType,
+            SafeMath.add(block.timestamp, _dateLimit),
+            _feePrice,
             false
         );
     } // Only dev contract // Deployed on Binance
 
-    function buyLotteryTicket(uint256 _lotteryTicket)
+    function buyStakingTicket(uint256 _lotteryTicket)
         public
         payable
         validEntry(_lotteryTicket)
     {
         lotteryBook[_lotteryTicket].participants.push(msg.sender);
+        totalFeeBalance = SafeMath.add(totalFeeBalance, msg.value);
     }
+
+    function buyLotteryTicket(uint256 _lotteryTicket, uint256 _slotNumber)
+        public
+        payable
+        validEntry(_lotteryTicket)
+    {
+        require(
+            _slotNumber < lotteryBook[_lotteryTicket].participants.length,
+            "Slot number is above supported"
+        );
+        require(
+            lotteryBook[_lotteryTicket].participants[_slotNumber] !=
+                address(0x0),
+            "Slot is taken already"
+        );
+        lotteryBook[_lotteryTicket].participants[_slotNumber] = msg.sender;
+        totalFeeBalance = SafeMath.add(totalFeeBalance, msg.value);
+    }
+
+    function setLotteryWinners(
+        uint256[] memory _winnerIndex,
+        uint256 _lotteryTicket
+    ) public {
+        require(
+            lotteryBook[_lotteryTicket].pickedWinners == false,
+            "Winners have already been picked."
+        );
+        for (uint256 i = 0; i < _winnerIndex.length; i++) {
+            lotteryBook[_lotteryTicket].winners.push(
+                lotteryBook[_lotteryTicket].participants[i]
+            );
+        }
+        lotteryBook[_lotteryTicket].pickedWinners = true;
+    } // Only dev contract // Deployed on Binance
+
+    // ************************** Contract fee withdraw *************************
+
+    function withdrawFunds() public payable {}
 }
